6cf74892238639a7bcfab6a6c9c8a4c5e9a29d75
EGeorge2021r <egeorge2013khs@gmail.com>
2022-01-24 15:59:37 -0600

    Coding the blockchain, run the streamlit app and screen capture

--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -50,7 +50,11 @@ import hashlib
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
 # YOUR CODE HERE
-
+@dataclass
+class Record: 
+    sender: str
+    receiver: str
+    amount: float
 
 ################################################################################
 # Step 2:
@@ -68,23 +72,30 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
-
+    #data: Any
+    record: Any
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
     nonce: int = 0
 
     def hash_block(self):
+        # Declare a hashing algorithm
         sha = hashlib.sha256()
 
+        # Turn the record into an encoded string
         record = str(self.record).encode()
+        # Add the encoded record into the hash
         sha.update(record)
 
+        # Turn the creator_id into an encoded string
         creator_id = str(self.creator_id).encode()
+        # Add the encoded creator_id into the hash
         sha.update(creator_id)
 
+        # Encode the time of record creation
         timestamp = str(self.timestamp).encode()
+        # Add the encoded record time into the hash
         sha.update(timestamp)
 
         prev_hash = str(self.prev_hash).encode()
@@ -93,11 +104,13 @@ class Block:
         nonce = str(self.nonce).encode()
         sha.update(nonce)
 
+        # Return the hash to the rest of the Block class
         return sha.hexdigest()
 
 
 @dataclass
 class PyChain:
+    # The class `PyChain` holds a list of blocks
     chain: List[Block]
     difficulty: int = 4
 
@@ -162,7 +175,9 @@ pychain = setup()
 # 2. Add an input area where you can get a value for `sender` from the user.
 # 3. Add an input area where you can get a value for `receiver` from the user.
 # 4. Add an input area where you can get a value for `amount` from the user.
-# 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
+# 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, 
+# which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include 
+# the attributes for `creator_id` and `prev_hash`.
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
@@ -171,14 +186,17 @@ input_data = st.text_input("Block Data")
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
 # YOUR CODE HERE
+sender = st.text_input("Input Sender Information")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
 # YOUR CODE HERE
+receiver = st.text_input("Input Receiver Information")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
 # YOUR CODE HERE
+amount = st.text_input("Input Amount Information")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,7 +207,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record = Record,
         creator_id=42,
         prev_hash=prev_block_hash
     )
