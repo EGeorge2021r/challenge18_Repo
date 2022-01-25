661882c0158a4d941cff2ac45549be5dfa25212c
EGeorge2021r <egeorge2013khs@gmail.com>
2022-01-24 21:25:23 -0600

    Updating codes and screenshot output, drag and drop in Github README file

--- Starter_Code/pychain.py
+++ Starter_Code/pychain.py
@@ -72,8 +72,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    #data: Any
-    record: Any
+    record: Record
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
@@ -213,7 +212,7 @@ if st.button("Add Block"):
     )
 
     pychain.add_block(new_block)
-    st.balloons()
+    #st.balloons()
 
 ################################################################################
 # Streamlit Code (continues)
